fa5a34807af76b4a923f884617a1933d2e877846
MiltonStanley <milton.stanley@gmail.com>
2013-01-12 10:14:18 -0600

    Tweaked include in note.cpp

--- libs/note.cpp
+++ libs/note.cpp
@@ -1,12 +1,13 @@
 //note.cpp
 #include <iostream>
-#include "./note.h"
+#include "note.h"
 
 using namespace std;
 
 int main() {
   Note one(440, 8);
   cout << "Frequency is " << one.getFrequency() << endl;
+
   return 0;
 
 
