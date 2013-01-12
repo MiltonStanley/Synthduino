8a1f0667f02308ad025758030dc516a5ed8ef35f
MiltonStanley <milton.stanley@gmail.com>
2013-01-12 10:39:02 -0600

    Different pitches testing

--- Synthduino.ino
+++ Synthduino.ino
@@ -2,9 +2,13 @@ void setup() {
  pinMode(12, OUTPUT);
 }
 
+// A440 = 2273;
+// Double = 4546
+
 void loop() {
   digitalWrite(12, HIGH);
-  delayMicroseconds(2273);
+  delayMicroseconds(4546);
   digitalWrite(12, LOW);
-  delayMicroseconds(2273);
+  delayMicroseconds(4546);
 }
+
