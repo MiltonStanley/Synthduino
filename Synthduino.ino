5fd97ec090c8371b6f9c8809ca89730ef57ae712
MiltonStanley <milton.stanley@gmail.com>
2013-01-12 14:04:39 -0600

    added frequency (clone of pitches.h)

--- Synthduino.ino
+++ Synthduino.ino
@@ -6,19 +6,12 @@ void setup() {
 
 int PITCH = 2273;
 
-Note a440(2273, 8);
-Note a220(4546, 8);
+Note a440(440, 8);
+Note a220(220, 8);
 
 // A440 = 2273;
 // Double = 4546
 
 void loop() {
   a220.play(12);
-}
-
-void Note::play(int pin){
-  digitalWrite(pin, HIGH);
-  delayMicroseconds(frequency);
-  digitalWrite(pin, LOW);
-  delayMicroseconds(frequency);
 }
--- note.h
+++ note.h
@@ -1,11 +1,29 @@
-//note.h
+#ifndef NOTE_H
+#define NOTE_H
+
+#include "Arduino.h"
 
 class Note{
  public:
-  Note(int _frequency, int _duration){frequency = _frequency; duration = _duration;};
+  Note(int _frequency, int _duration);//{frequency = _frequency; duration = _duration;};
   ~Note(){};
   int getFrequency(){return frequency;};
   void play(int pin);
-  int frequency;
-  int duration;
+  int frequency;  // in Hz
+  int period;     // in microseconds
+  int duration;   // Music value
 };
+
+Note::Note(int _frequency, int _duration) {
+  frequency = _frequency;
+  period = (1.0/frequency) * 1000000; // Period = reciprical of frequency; then convert to milliseconds
+  duration = _duration;
+}
+
+void Note::play(int pin){
+  digitalWrite(pin, 1);
+  delayMicroseconds(frequency);
+  digitalWrite(pin, 0);
+  delayMicroseconds(frequency);
+}
+#endif
