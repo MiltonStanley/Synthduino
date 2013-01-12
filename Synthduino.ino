02d1a9c1715c2b134f2ede5039cd0496c4b949e2
MiltonStanley <milton.stanley@gmail.com>
2013-01-12 15:15:36 -0600

    Working on play timer

--- Synthduino.ino
+++ Synthduino.ino
@@ -8,5 +8,11 @@ void setup() {
 Note theNote(NOTE_A4, 8);
 
 void loop() {
-  theNote.playDelay(12);
+  //theNote.playDelay(12);
+  theNote.playTimer(12);
 }
+
+// 120 bmp = 120 qNotes/60 seconds = 2 Hz freq, period = .5 sec = 500k millis
+// 8 = 4hz = .25 * 1,000,000 = 250,000
+
+// whole note  = .5 Hz
--- note.h
+++ note.h
@@ -3,12 +3,13 @@
 
 #include "Arduino.h"
 
-class Note{
+class Note {
  public:
   Note(int _frequency, int _duration);//{frequency = _frequency; duration = _duration;};
   ~Note(){};
   int getFrequency(){return frequency;};
   void playDelay(int pin);
+  void playTimer(int pin);
   int frequency;  // in Hz
   int period;     // in microseconds
   int duration;   // Music value
@@ -18,12 +19,36 @@ Note::Note(int _frequency, int _duration) {
   frequency = _frequency;
   period = (1.0/frequency) * 1000000; // Period = reciprical of frequency; then convert to milliseconds
   duration = _duration;
+  length = (1.0/duration) * 1000000; // Note ends length microseconds after it starts
 }
 
-void Note::playDelay(int pin){
+void Note::playDelay(int pin) {
   digitalWrite(pin, HIGH);
   delayMicroseconds(period);
   digitalWrite(pin, LOW);
   delayMicroseconds(period);
 }
+
+void Note::playTimer(int pin) {
+  int start = micros();       // Current time in microseconds
+  bool state = HIGH;          // keep track of current pin state (set high)
+  int end = start + length;
+  digitalWrite(pin, state);    // Pin goes HIGH
+  int toggle = start + period;  // Should stay high until toggle (start + period)
+  while (micros() < end){
+    if (micros() > toggle) {      // If micros > toggle, need to toggle
+      state = !state;
+      digitalWrite(pin, state);
+      toggle = toggle + period;
+    }
+  }
+}
+
 #endif
+
+
+/* start = 1000
+ * state = HIGH
+ * write high
+ * toggle = start + period = 1000 + 2500 = 3500
+ * if micros > 
