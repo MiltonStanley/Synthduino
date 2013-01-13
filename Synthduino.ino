b6b9ab65e583cc239a85788ef31f670af9ae86bd
MiltonStanley <milton.stanley@gmail.com>
2013-01-13 15:20:47 -0600

    Much nicer array building now

--- Synthduino.ino
+++ Synthduino.ino
@@ -1,43 +1,26 @@
 #include "note.h"
 #include "pitches.h"
-#define LENGTH 32
 
-/* 
-// Alberti Baseline
-int notes[] = {NOTE_C4, NOTE_G4, NOTE_E4, NOTE_G4,
-							 NOTE_C4, NOTE_G4, NOTE_E4, NOTE_G4,
-							 NOTE_B3, NOTE_G4, NOTE_F4, NOTE_G4,
-							 NOTE_C4, NOTE_G4, NOTE_E4, NOTE_G4,
-							 NOTE_C4, NOTE_A4, NOTE_F4, NOTE_A4,
-							 NOTE_C4, NOTE_G4, NOTE_E4, NOTE_G4,
-							 NOTE_B3, NOTE_G4, NOTE_F4, NOTE_G4,
-							 NOTE_C4, NOTE_G4, NOTE_E4, NOTE_G4					 
-							};
-int durations[] = { 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
-										4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4				
-									};
+#define BASS 12
+
+/* HEADER SECTION
+ * Declare your tempo and parts.
+ * Examples:
+ * TEMPO = 120 // in bpm (beats per minute) - how many quarter notes per minute
+ * Note bassline[] = { {NOTE_C3, 2}, {NOTE_DS3, 2}, {NOTE_F3, 1} };
  */
 
-Note melody[LENGTH];
-Note theNote(NOTE_A4, 2);
-  
+Note bassLine[] = { {NOTE_C3, 2}, {NOTE_DS3, 2}, {NOTE_F3, 1} };
+
+#define TEMPO 120
+
 void setup(){
-  pinMode(12, OUTPUT);
-  for(int i=0; i<LENGTH; i++){
-    Note temp(notes[i], durations[i]);
-    melody[i] = temp;
-  }
+	pinMode(BASS, OUTPUT);
 }
 
 void loop() {
-  for(int i=0; i<LENGTH; i++){
-    melody[i].play(12);
+  for(int i=0; i<3; i++){
+    bassLine[i].play(12);
   }
  // delay(10000);
- //theNote.play(12); 
 }
-
-// 120 bmp = 120 qNotes/60 seconds = 2 Hz freq, period = .5 sec = 500k millis
-// 8 = 4hz = .25 * 1,000,000 = 250,000
-
-// whole note  = .5 Hz
