a77ffe1143ac94e9baf700759ccede85faed57a9
MiltonStanley <milton.stanley@gmail.com>
2013-01-16 17:25:22 -0600

    Working on enharmonics

--- tools/conversion.rb
+++ tools/conversion.rb
@@ -3,7 +3,19 @@ NEW_NAME = 'pitches.h'
 old_file = File.open('../pitches.h', 'r')
 new_file = File.new(NEW_NAME, 'w')
 
+class String
+
+	def enharmonic?
+		self.include? 'S'
+	end
+
+	def transpose
+		
+
+end
+
 while line = old_file.gets
 	puts line
 	puts line.gsub!('NOTE_','n')
+	puts line.enharmonic?
 end
