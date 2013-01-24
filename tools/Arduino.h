81d857bf6ff51aa995c9f2933aa0d9e0905124d3
MiltonStanley <milton.stanley@gmail.com>
2013-01-24 16:49:50 -0600

    Just gonna dump data by Serial, process in Ruby

--- tools/Arduino.h
+++ tools/Arduino.h
@@ -10,18 +10,13 @@ using namespace std;
 
 void digitalWrite(int pin, int voltage) {
 	cout << " Setting pin " << pin << " " << (voltage ? "HIGH" : "LOW");
-	if (voltage) noteFrequency = noteFrequency++;
 }
 
 void delayMicroseconds(int microseconds) {
-	clock_t timer = clock();
-	cout << clock();
-	cout << " Delaying " << microseconds << " microseconds" << endl;
-	while (clock() > (timer + microseconds)){cout << "Delay" << endl;};
-	cout << clock();
+  
 }
 
 long micros() {
-	clock_t microsecondsSinceStart = clock();
-	return clock();
+  long x = 1000;
+  return x;
 }
